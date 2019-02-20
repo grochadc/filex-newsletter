d32e070d2709a935fcddb430e529e26908fdb118
Gonzalo Rocha <grochadc@gmail.com>
2019-02-19 21:49:03 -0600

    Add cli arguments support

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
-const nodemailer = require("nodemailer");
-
-const fs = require("fs");
-const { parseCSS } = require("./parseCSS");
-
-async function main() {
-  console.log("creating test account...");
-  let account = await nodemailer.createTestAccount();
-
-  let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: account.user, // generated ethereal user
-      pass: account.pass // generated ethereal password
+const { sendEmail } = require("./sendEmail");
+require("yargs")
+  .usage("$0 <cmd> [args]")
+  .command(
+    "send",
+    "send the email",
+    {
+      user: {
+        alias: "u"
+      },
+      pass: {
+        alias: "p"
+      },
+      server: {
+        alias: "s"
+      }
     },
-    tls: {
-      rejectUnauthorized: false
+    function(argv) {
+      sendEmail({
+        user: argv.user,
+        pass: argv.pass,
+        server: argv.server
+      }).catch(console.error);
     }
-  });
-
-  console.log("Parsing css");
-  const parsedHTML = await parseCSS();
-
-  let mailOptions = {
-    from: "FILEX Newsletter <filex@cusur.udg.mx>",
-    to: "grochadc@gmail.com",
-    subject: parsedHTML.title,
-    html: parsedHTML.html
-  };
-  console.log("sending email...");
-  console.log(`HTML body: ${mailOptions.html}`);
-  let info = await transporter.sendMail(mailOptions);
-  console.log("Sent email");
-  console.log(`Message sent: ${info.messageId}`);
-  console.log(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
-}
-
-main().catch(console.error);
+  )
+  .help().argv;
---
+++ sendEmail.js
@@ -0,0 +1,62 @@
+const nodemailer = require("nodemailer");
+
+const fs = require("fs");
+const { parseCSS } = require("./parseCSS");
+
+async function main(cli) {
+  let transportOpts;
+
+  if (cli.user && cli.pass && cli.server) {
+    console.log("Got info from arguments!");
+    transportOpts = {
+      host: cli.server,
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: cli.user,
+        pass: cli.pass
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    };
+  } else {
+    console.log("Creating test account...");
+    const account = await nodemailer.createTestAccount();
+    transportOpts = {
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: account.user,
+        pass: account.pass
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    };
+  }
+
+  console.log("Creating transporter");
+  let transporter = nodemailer.createTransport(transportOpts);
+
+  console.log("Parsing css");
+  const parsedHTML = await parseCSS();
+
+  let mailOptions = {
+    from: "FILEX Newsletter <filex@cusur.udg.mx>",
+    to: "grochadc@gmail.com",
+    subject: parsedHTML.title,
+    html: parsedHTML.html
+  };
+  console.log("sending email...");
+  console.log(`HTML body: ${mailOptions.html}`);
+  let info = await transporter.sendMail(mailOptions);
+  console.log("Sent email");
+  console.log(`Message sent: ${info.messageId}`);
+  console.log(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
+}
+
+module.exports = {
+  sendEmail: main
+};
