02121ef71d03271fa212873cdd07ffd232790a68
Gonzalo Rocha <grochadc@gmail.com>
2019-03-29 13:43:55 -0600

    Move templates js to own folder

---
+++ newsletter/templates/4hedy-lamarr.template.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: "FILEX Newsletter #4: Hedy Lamarr",
+  subtitle: "Hedy Lamarr, the actress who invented Wifi",
+  image:
+    "https://filex-newsletter-img.netlify.com/4hedy-lamarr/hedy-lamarr.jpg",
+  body: `
+  <p>SHE was considered “the most beautiful woman in the world”, and had one of the most iconic faces of the 1930s and 1940s Hollywood. She provided the inspiration for Snow White and Catwoman. Other actresses tried to look like her, and leading men sought<sup>1</sup> to be with her. But Hedy Lamarr’s glamorous career overshadows<sup>2</sup> her most significant achievements.</p>
+  <p>Born in 1914 to a Jewish family, Hedwig Eva Maria Kiesler grew up absorbing the high culture and she spoke 4 languages. Her father was a bank director with a keen<sup>3</sup> curiosity for the world around him, and she inherited his inquisitive mindset.</p>
+  <p>She appeared in a famous 1934 film called Ecstasy, this film send her to stardom immediately, not just because of her looks, but also because she was the first woman to act an orgasm in the history of cinema, a few years later, she married to a wealthy Nazi weapon<sup>4</sup> manufacturer, an extremely controlling husband prevented her from pursuing her acting career. She claimed she was kept a virtual prisoner in their castle home, from which she could escape disguised as her maid.</p>
+  <p>She went to the US and began a career as an actress being a huge success being considered the most beautiful woman in the world.</p>
+  <p>During World War II, Lamarr learned that radio-controlled torpedoes could easily be jammed and set off course. She thought of creating a frequency-hopping signal that could not be tracked or jammed<sup>5</sup>. She succeeded by synchronizing a miniaturized piano mechanism with radio signals. This is considered an early form of Wifi, unfortunately, she is more remembered because for her beauty than her inventions, which we still use today.</p>
+  <p>In the last decades of her life, the telephone became Lamarr's only means of communication with the outside world, even with her children and close friends. Lamarr died in Casselberry, Florida, on January 19, 2000, of heart disease, aged 85.</p>
+  `,
+  vocabulary: [
+    "Sought: wanted",
+    "Overshadows: make less important",
+    "Keen: enthusiastic",
+    "Weapon: gun",
+    "Jammed: ruined"
+  ]
+};
