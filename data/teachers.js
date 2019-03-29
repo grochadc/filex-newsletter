b275672c398b94e8a017a6d8e87051755e058d7e
Gonzalo Rocha <grochadc@gmail.com>
2019-03-29 13:04:49 -0600

    Add teachers emails.

---
+++ data/teachers.js
@@ -0,0 +1,14 @@
+module.exports = [
+  "tachorangel@hotmail.com",
+  "brenda_ldts@hotmail.com",
+  "carlosmtzos@gmail.com",
+  "eriqa.roal@gmail.com",
+  "origamif@hotmail.com",
+  "flor.rodriguezl@hotmail.com",
+  "gissel.cerrillos@gmail.com",
+  "grochadc@gmail.com",
+  "jessy_z_1992@hotmail.com",
+  "j.arias1@yahoo.com",
+  "pca_por@hotmail.com",
+  "tvizcaino7789@gmail.com"
+];
