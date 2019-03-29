c99ce92d7390bc6390d163ced894f6845cfc708a
Gonzalo Rocha <grochadc@gmail.com>
2019-03-29 13:04:35 -0600

    3rd article template executed

---
+++ newsletter/3pink-city.template.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: "FILEX Newsletter #3: The Pink City",
+  subtitle: `Why Jaipur’s King Painted His City Pink to Impress the Prince of Wales`,
+  image: "https://filex-newsletter-img.netlify.com/3pink-city/image1.jpg",
+  body: `
+    <p>If a member of the royal family were coming to your home, you’d probably spruce<sup>1</sup> it up a bit—maybe rearrange the furniture and plop some peonies into a vase. The king of Jaipur, however, went above and beyond<sup>2</sup> what's expected of a host.</p>
+    <p>In an effort to impress the Prince of Wales ahead of his state visit in 1876, it is widely believed that the king had the entire city painted pink.</p>
+    <p>Nestled<sup>3</sup> in the state of Rajasthan in northern India, Jaipur is about a one-hour flight from New Delhi. Today, this popular tourist site is affectionately known as the “Pink City,” but its streets weren’t always so rosy. Prior<sup>4</sup> to a state visit from Prince Albert Edward—the eldest son of Queen Victoria and her consort, Prince Albert—Jaipur’s buildings were either white or a “sallow yellow,” according to The Rough Guide to India.</p>
+    <p>In hopes of dazzling<sup>5</sup> his royal counterpart, the reigning maharaja (“great king”) of Jaipur, Sawai Ram Singh II, decided to undertake something of a remodeling project. He ordered all buildings in the city to be painted the same shade of pink—a color that symbolizes hospitality. At the urging<sup>6</sup> of his favorite wife, the maharaja took it one step further and passed a law in 1877 making it illegal for buildings in the old city to be painted any color other than "Jaipur pink." This law still remains in effect today.</p>
+    <p>According to one account, the first person to call Jaipur the “Pink City” was writer Stanley Reed, a correspondent for The Times of India who wrote about the Prince of Wales’s royal visit.</p>
+  `,
+  vocabulary: [
+    "Spruce: clean",
+    "Above and beyond: much more than needed",
+    "Nestled: Situated",
+    "Prior: before",
+    "Dazzling: impress",
+    "Urging: pressure"
+  ]
+};
